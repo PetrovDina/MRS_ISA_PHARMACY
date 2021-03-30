129d080e22a437512b13e2c526d0aee36f0724f8
tosic-sw <veljkozajecar@gmail.com>
2021-03-30 19:23:06 +0200

    Povezan front i back kod registracije apoteka. Dummy podaci.

--- pharmacy/src/main/resources/data-postgres.sql
+++ pharmacy/src/main/resources/data-postgres.sql
@@ -7,6 +7,7 @@ insert into medication (name, manufacturer, prescriptionReq, form) values ('Pank
 insert into location (latitude, longitude, street, city, zipcode, streetNum) values (30.30,20.20, 'Narodnog fronta','Novi Sad', '21000', 5);
 insert into location (latitude, longitude, street, city, zipcode, streetNum) values (30.30,20.20, 'Bulevar Oslobođenja','Novi Sad', '21000', 88);
 insert into location (latitude, longitude, street, city, zipcode, streetNum) values (30.30,20.20, 'Jevrejska','Novi Sad', '21000', 21);
+insert into location (latitude, longitude, street, city, zipcode, streetNum) values (30.30,20.20, 'Ljube Nesice','Zajecar', '19000', 21);
 
 insert into pharmacy (name, rating, location_id) values ('Apoteka Jankovic', 2.5, 1);
 insert into pharmacy (name, rating, location_id) values ('Benu', 4.5, 2);
