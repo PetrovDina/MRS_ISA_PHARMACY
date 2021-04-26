ccdf49fac02872a3d98c6371cbbdc90d76590c82
Dina Petrov <dina.petrov@gmail.com>
2021-04-26 17:40:11 +0200

    Sitne izmene

--- pharmacy-front/src/views/MedicationReservationView.vue
+++ pharmacy-front/src/views/MedicationReservationView.vue
@@ -7,7 +7,7 @@
             :results="results"
             @pharmacySelected="chosenPharmacySelected"
         ></PharmaciesWithPriceComponent>
-        <br>
+        <br />
         <div>
             <span>
                 <input
@@ -16,42 +16,135 @@
                     class="form-control"
                     id="quantityInput"
                     placeholder="Enter quantity"
-                    min = 0
-                    :max="[chosenRow? chosenRow.availableQuantity : 100]"
+                    min="0"
+                    :max="[chosenRow ? chosenRow.availableQuantity : 100]"
                 />
             </span>
 
             <span>
                 <label for="dueDate">Pick up due date</label>
-                <input type="date" id="dueDate" name="dueDate" :min="today"
-                    class="form-control" v-model="chosenDueDate"/>
+                <input
+                    type="date"
+                    id="dueDate"
+                    name="dueDate"
+                    :min="today"
+                    class="form-control"
+                    v-model="chosenDueDate"
+                />
             </span>
-            <br><br><br>
-            <button class="cancelButton btn btn-secondary" @click="goBack()"> Cancel </button>
-            <button class="finishButton btn btn-primary" @click="finishReservation()"> Complete reservation </button>
+            <br /><br /><br />
+            <button class="cancelButton btn btn-secondary" @click="goBack()">
+                Cancel
+            </button>
+            <button
+                class="finishButton btn btn-primary"
+                @click="finishReservation()"
+            >
+                Complete reservation
+            </button>
 
+            <!-- Modal -->
+            <div
+                class="modal fade"
+                id="exampleModal"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        
+                        <div class="modal-body">Please fill all fields!</div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                            >
+                                Ok
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                        <!-- Modal -->
+            <div
+                class="modal fade"
+                id="exampleModal2"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        
+                        <div class="modal-body">Please input valid quantity!</div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                            >
+                                Ok
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                                    <!-- Modal -->
+            <div
+                class="modal fade"
+                id="exampleModal3"
+                tabindex="-1"
+                role="dialog"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true"
+            >
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        
+                        <div class="modal-body">Successfully reserved!</div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-dismiss="modal"
+                                @click="goBack()"
+                            >
+                                Go back
+                            </button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import PharmaciesWithPriceComponent from "../components/PharmaciesWithPriceComponent.vue";
-import moment from 'moment'
+import moment from "moment";
 import { client } from "@/client/axiosClient";
-
+import $ from 'jquery'
 
 export default {
     name: "MedicationReservationView",
 
-    components: { PharmaciesWithPriceComponent  },
+    components: { PharmaciesWithPriceComponent },
 
     props: {
         medication: {
             type: Object,
             required: false,
-            default: null
+            default: null,
         },
-
     },
 
     data() {
@@ -65,77 +158,76 @@ export default {
             chosenDueDate: null,
 
             today: moment().format("YYYY-MM-DD"),
-
         };
     },
 
     methods: {
-
         chosenPharmacySelected(row) {
             this.chosenQuantity = null;
             this.chosenRow = row;
         },
 
-        finishReservation(){
-
-            if (!this.chosenRow || !this.chosenQuantity || !this.chosenDueDate){
-                alert("Please select all fields");
-                //todo modalni prozor ovde umesto alert
+        finishReservation() {
+            if (
+                !this.chosenRow ||
+                !this.chosenQuantity ||
+                !this.chosenDueDate
+            ) {
+                $("#exampleModal").modal('show');
                 return;
             }
 
-            if (this.chosenQuantity == 0 || this.chosenQuantity > this.chosenRow.availableQuantity){
-                alert("Please enter valid quantity!");
-                //todo modalni prozor ovde umesto alert!
+            if (
+                this.chosenQuantity == 0 ||
+                this.chosenQuantity > this.chosenRow.availableQuantity
+            ) {
+                $("#exampleModal2").modal('show');
                 return;
             }
 
-            
             let p = {
-                id : 2, //ovo je samo bitno, ostalo nije jer ce na osnovu id-a na beku da se vadi iz baze
+                id: 2, //ovo je samo bitno, ostalo nije jer ce na osnovu id-a na beku da se vadi iz baze
                 username: "pera",
                 password: "pera",
                 email: "pera@gmail.com",
                 firstName: "pera",
                 lastName: "peric",
-                penaltyPoints: 0
+                penaltyPoints: 0,
             };
 
             client({
-                    url: "reservation/create",
-                    method: "POST",
-                    data: { 
-                        patient: p,
-                        pharmacy: this.chosenRow.pharmacy,
-                        medication: this.medication,
-                        quantity: this.chosenQuantity,
-                        dueDate: this.chosenDueDate
+                url: "reservation/create",
+                method: "POST",
+                data: {
+                    patient: p,
+                    pharmacy: this.chosenRow.pharmacy,
+                    medication: this.medication,
+                    quantity: this.chosenQuantity,
+                    dueDate: this.chosenDueDate,
+                },
+            }).then((response) => console.log(response.data));
 
-                    }
-            }).then((response) => (console.log(response.data)));
-            //todo add modal here that says success
-            this.$router.push({name:"Home"});
+            $("#exampleModal3").modal('show');
 
+            //this.$router.push({ name: "Home" });
         },
 
-        goBack(){
-            this.$router.push({name:"Home"});
-
-        }
+        goBack() {
+            this.$router.push({ name: "Home" });
+        },
     },
-
 
     mounted() {
         //check if medication passed as prop
         //we need this in case someone presses the back or forward button
-        if (!this.medication){
-            this.$router.push({name:"Home"});
+        if (!this.medication) {
+            this.$router.push({ name: "Home" });
             return;
         }
 
         client({
             url: "pharmacyStorageItem/allByMedicationAndQuantity",
-            params: {medicationId: this.medication.id},
+            params: { medicationId: this.medication.id },
             method: "GET",
         }).then((response) => (this.results = response.data));
     },
@@ -154,26 +246,24 @@ span {
 }
 
 .form-control:focus {
-        border-color: rgba(155, 82, 151, 0.527);
-        box-shadow: 0 0 0 0.1rem rgba(155, 82, 151, 0.527);;
+    border-color: rgba(155, 82, 151, 0.527);
+    box-shadow: 0 0 0 0.1rem rgba(155, 82, 151, 0.527);
 }
 
 .finishButton {
-    background-color: rgba(15, 95, 72, 0.95);;
+    background-color: rgba(15, 95, 72, 0.95);
     border-color: rgba(15, 95, 72, 0.95);
 }
 
-#reservationDiv{
+#reservationDiv {
     margin: 100px 60px 30px 60px;
 }
 
-p{
+p {
     font-size: 20px;
 }
 
-.title{
+.title {
     font-size: 24px;
-
 }
-
 </style>
