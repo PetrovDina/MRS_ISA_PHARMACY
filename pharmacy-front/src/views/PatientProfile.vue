2680dec6bd3599bf34a445cfd1fa9e91a11cd67d
Dina Petrov <dina.petrov@gmail.com>
2021-05-14 18:42:57 +0200

    Edit profila pacijenta i menjanje lozinke

--- pharmacy-front/src/views/PatientProfile.vue
+++ pharmacy-front/src/views/PatientProfile.vue
@@ -1,32 +1,44 @@
 <template>
     <div id="profileDiv">
-        <p class = "titl"> My profile </p>
-        <div id="flexDiv" v-if="user" >
+        <p class="titl">My profile</p>
+        <div id="flexDiv" v-if="user">
             <div id="sideBar" class="card flex-card">
                 <div id="pictureDiv">
-                    <img src="../assets/profilePicture.png">
-
+                    <img src="../assets/profilePicture.png" />
                 </div>
                 <div id="userInfoDiv">
-                    <p><b>Username: </b>{{user.username}}</p>
-                    <p><b>Name: </b>{{user.firstName}} {{user.lastName}}</p>
-                    <p><b>Email: </b>{{user.email}}</p>
-                    <p><b>Address: </b>{{user.location.street}} {{user.location.streetNum}}, {{user.location.city}}</p>
+                    <p><b>Username: </b>{{ user.username }}</p>
+                    <p><b>Name: </b>{{ user.firstName }} {{ user.lastName }}</p>
+                    <p><b>Email: </b>{{ user.email }}</p>
+                    <p>
+                        <b>Address: </b>{{ user.location.street }}
+                        {{ user.location.streetNum }}, {{ user.location.city }}
+                    </p>
                 </div>
             </div>
             <div id="optionsBar" class="card flex-card">
                 <TabNav
-                    :tabs="['Edit profile', 'Allergies','Subscriptions', 'Penalty system']"
+                    :tabs="[
+                        'Edit profile',
+                        'Change password',
+                        'Allergies',
+                        'Subscriptions',
+                        'Penalty system',
+                    ]"
                     :selected="selected"
                     @selected="setSelected"
                 >
                     <Tab :isSelected="selected === 'Edit profile'">
+                        <PatientProfileEdit></PatientProfileEdit>
+                    </Tab>
+
+                    <Tab :isSelected="selected === 'Change password'">
+                        <PasswordChangeComponent></PasswordChangeComponent>
                     </Tab>
 
                     <Tab :isSelected="selected === 'Allergies'">
                         <AllergiesAddEdit></AllergiesAddEdit>
                     </Tab>
-
                 </TabNav>
             </div>
         </div>
@@ -40,19 +52,20 @@ import Button from "@/components/Button";
 import TabNav from "../components/TabNav.vue";
 import Tab from "../components/Tab.vue";
 import AllergiesAddEdit from "../components/AllergiesAddEdit.vue";
+import PatientProfileEdit from "../components/PatientProfileEdit.vue";
+import PasswordChangeComponent from "../components/PasswordChangeComponent.vue";
 
 import $ from "jquery";
 
 export default {
     name: "PatientProfile",
 
-    components: { Button, TabNav, Tab, AllergiesAddEdit },
-
+    components: { Button, TabNav, Tab, AllergiesAddEdit, PatientProfileEdit, PasswordChangeComponent },
 
     data() {
         return {
             user: null,
-            selected: 'Edit profile',
+            selected: "Edit profile",
         };
     },
 
@@ -85,7 +98,8 @@ export default {
     margin: 15px;
     border-color: rgba(142, 141, 141, 0.694);
     /* width: 100%; */
-    /* height: 100%; */
+    height: 100%;
+    min-height: 50vh;
 }
 #flexDiv {
     display: flex;
--- pharmacy/src/main/java/mrsisa12/pharmacy/controller/UserController.java
+++ pharmacy/src/main/java/mrsisa12/pharmacy/controller/UserController.java
@@ -3,19 +3,26 @@ package mrsisa12.pharmacy.controller;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import mrsisa12.pharmacy.dto.LoginDTO;
 import mrsisa12.pharmacy.dto.UserTokenStateDTO;
+import mrsisa12.pharmacy.model.Patient;
 import mrsisa12.pharmacy.model.User;
 import mrsisa12.pharmacy.service.UserService;
 import mrsisa12.pharmacy.utils.TokenUtils;
@@ -32,6 +39,10 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 
 	// Prvi endpoint koji pogadja korisnik kada se loguje.
 	// Tada zna samo svoje korisnicko ime i lozinku i to prosledjuje na backend.
@@ -56,6 +67,47 @@ public class UserController {
 
 		// Vrati token kao odgovor na uspesnu autentifikaciju
 		return ResponseEntity.ok(new UserTokenStateDTO(jwt, expiresIn));
+	}
+	
+	@GetMapping(value = "/usernameTaken/{username}")
+	public ResponseEntity<Boolean> usernameTaken(@PathVariable("username") String username) {
+
+		User user = userService.findByUsername(username);
+		
+		if (user == null) {
+			return new ResponseEntity<>(false, HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(true, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/tryChangePassword")
+	public ResponseEntity<Boolean> tryChangePassword(@RequestParam("username") String username, @RequestParam("oldPassword") String oldPassword,
+			@RequestParam("newPassword") String newPassword) {
+
+		User user = userService.findByUsername(username);
+		
+		if (user == null) {
+			return new ResponseEntity<>(false, HttpStatus.OK);
+		}
+		
+		//provera sifre i postavljanje nove!
+		try {
+			Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					username, oldPassword));
+			
+			//ispravna lozinka
+			user.setPassword(passwordEncoder.encode(newPassword));
+			userService.save(user);
+			
+			
+			
+		}catch (AuthenticationException e) {
+			return new ResponseEntity<>(false, HttpStatus.OK); //neispravna lozinka
+		}
+		
+
+		return new ResponseEntity<>(true, HttpStatus.OK);
 	}
 
 }
