ea4ffdd48a2b25baa6be2f512c3babb1865cdd21
Bojan <58345648+BoJaN77799@users.noreply.github.com>
2021-06-14 21:25:54 +0200

    axiosClient.js updated path

--- pharmacy-front/src/client/axiosClient.js
+++ pharmacy-front/src/client/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://mrs-isa-pharmacy.herokuapp.com/',
 });
 
 client.interceptors.request.use(
